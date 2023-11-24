c4c8b68faba2afb9db865a5ffeb442a394b946b9
Semyon Chaykin <52384223+thecakeisfalse@users.noreply.github.com>
2023-11-24 16:19:07 +0300

    [АиСД] Задача "fibsys"

---
+++ algorithms and data structures/fibsys.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+typedef unsigned long long ullong;
+
+int main() {
+    ullong x, i = 2, a[100] = {0, 1};
+    scanf("%llu", &x);
+
+    for (i = 2; (a[i] = a[i-1] + a[i-2]) <= x; i++);
+
+    if (x == 0)
+        printf("0");
+
+    while (i > 2) {
+        printf("%d", a[--i] <= x);
+        x -= a[i] * (a[i] <= x);
+    }
+
+    return 0;
+}
