82a2df60ab0d57946c6272c14147287ecbb59871
Semyon Chaykin <52384223+thecakeisfalse@users.noreply.github.com>
2023-11-26 15:11:29 +0300

    [АиСД] Задача "permut"

---
+++ algorithms and data structures/permut.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+void main() {
+    int a[8] = {0}, i = 0, j = 0, x;
+    for (i = 0; i < 8; ++i) scanf("%d", &a[i]);
+    for (i = 0; i < 8 && j != 8; ++i, a[j] = 1e9)
+        for (scanf("%d", &x), j = 0; j < 8 && a[j] != x; ++j);
+    printf("%s\n", (i == 8 && j != 8 ? "YES" : "NO"));
+}
