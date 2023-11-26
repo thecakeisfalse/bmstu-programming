3d2b42231abccc164a9420b1dffc210b8a8137e4
Semyon Chaykin <52384223+thecakeisfalse@users.noreply.github.com>
2023-11-26 16:57:06 +0300

    [АиСД] Задача "saddlepoint"

---
+++ algorithms and data structures/saddlepoint.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    int n, m, x; scanf("%d %d", &n, &m);
+    int rows[n], columns[m];
+
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < m; ++j) {
+            scanf("%d", &x);
+            columns[j] = (i == 0 ? x : fmin(columns[j], x));
+            rows[i] = (j == 0 ? x : fmax(rows[i], x));
+        }
+    }
+
+    int max = 0, min = 0;
+
+    for (int i = 0; i < n; ++i)
+        if (rows[i] < rows[max])
+            max = i;
+
+    for (int i = 0; i < m; ++i)
+        if (columns[i] > columns[min])
+            min = i;
+
+    if (columns[min] == rows[max])
+        printf("%d %d\n", max, min);
+    else
+        printf("none\n");
+
+    return 0;
+}
