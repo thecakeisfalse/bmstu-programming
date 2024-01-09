d37bb935c373c4a240b564742f3755786339f98f
Semyon Chaykin <52384223+thecakeisfalse@users.noreply.github.com>
2024-01-09 09:52:12 +0300

    [АиСД] Задача "power2"

---
+++ algorithms and data structures/power2.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int is_power2(int n) {
+    return n > 0 && (n & (n - 1)) == 0;
+}
+
+int main() {
+    int n; scanf("%d", &n);
+    int a[n];
+    for (int i = 0; i < n; ++i)
+        scanf("%d", &a[i]);
+
+    int summ = 0, count = 0;
+    for (int i = 0; i < (1 << n); ++i) {
+        summ = 0;
+        for (int j = 0; j < n; ++j)
+            if (i & (1 << j))
+                summ += a[j];
+
+        count += is_power2(summ);
+    }
+
+    printf("%d\n", count);
+
+    return 0;
+}
