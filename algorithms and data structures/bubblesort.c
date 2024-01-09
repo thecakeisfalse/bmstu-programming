a1b75db15827f6c33ca16fb1d733cf0b0b2d54e4
Semyon Chaykin <52384223+thecakeisfalse@users.noreply.github.com>
2024-01-09 09:52:22 +0300

    [АиСД] Задача "bubblesort"

---
+++ algorithms and data structures/bubblesort.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void bubblesort(unsigned long nel, int (*compare)(unsigned long i, unsigned long j), void (*swap)(unsigned long i, unsigned long j)) {
+    for (int i = 0; nel > 0 && i < nel-i-1; ++i) {
+        for (int j = i; j < nel-i-1; ++j)
+            if (compare(j, j+1) > 0)
+                swap(j, j+1);
+
+        for (int j = nel-i-1; j > i; --j)
+            if (compare(j, j-1) < 0)
+                swap(j, j-1);
+    }
+}
