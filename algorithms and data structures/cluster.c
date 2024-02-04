5f809206360ff5ee9a447a230bc92c1870fa394e
Semyon Chaykin <52384223+thecakeisfalse@users.noreply.github.com>
2024-02-05 02:44:27 +0300

    [АиСД] Упростил решение "cluster"

--- algorithms and data structures/cluster.c
+++ algorithms and data structures/cluster.c
@@ -1,163 +1,89 @@
-#include <stdbool.h>
+// https://neerc.ifmo.ru/wiki/index.php?title=Двоичная_куча
+
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-// #include <assert.h>
-#include <math.h>
-#include <stdint.h>
 
-#define swap(a, b)                                                             \
-    {                                                                          \
-        struct Item *temp = a;                                                 \
-        a = b;                                                                 \
-        b = temp;                                                              \
-    }
-
-struct Item {
-    int index, key;
-    int end;
-};
-
-struct PriorityQueue {
-    int capacity;
-    int count;
-    struct Item **heap;
-};
-
-struct Item *create_item(int key, int end) {
-    struct Item *item = (struct Item *)calloc(1, sizeof(struct Item));
-
-    item->key = key;
-    item->end = end;
-    item->index = -1;
-
-    return item;
+void swap(int *a, int *b) {
+    *a ^= *b;
+    *b ^= *a;
+    *a ^= *b;
 }
 
-void heapify(struct Item **a, int n, size_t i) {
-    while (1) {
-        size_t l = 2 * i + 1, r = 2 * i + 2, j = i;
+int max(int a, int b) { return a > b ? a : b; }
 
-        if (l < n && a[i]->end > a[l]->end)
-            i = l;
-        if (r < n && a[i]->end > a[r]->end)
-            i = r;
-        if (i == j)
+typedef struct pqueue {
+    int count;
+    int *heap;
+} pqueue_t;
+
+pqueue_t *init_pqueue(int n) {
+    pqueue_t *q = malloc(sizeof(pqueue_t));
+    q->count = 0;
+    q->heap = calloc(n, sizeof(int));
+    return q;
+}
+
+void delete_pqueue(pqueue_t *q) {
+    free(q->heap);
+    free(q);
+}
+
+void siftDown(pqueue_t *q, int i) {
+    while (2 * i + 1 < q->count) {
+        int l = 2 * i + 1, r = 2 * i + 2, j = l;
+
+        if (r < q->count && q->heap[r] < q->heap[l])
+            j = r;
+
+        if (q->heap[i] <= q->heap[j])
             break;
 
-        swap(a[i], a[j]);
-        a[i]->index = i;
-        a[j]->index = j;
+        swap(&q->heap[i], &q->heap[j]);
+        i = j;
     }
 }
 
-struct PriorityQueue *init_priority_queue(int n) {
-    struct PriorityQueue *queue =
-        (struct PriorityQueue *)calloc(1, sizeof(struct PriorityQueue));
-
-    queue->count = 0;
-    queue->capacity = n;
-    queue->heap =
-        (struct Item **)calloc(queue->capacity, sizeof(struct Item *));
-
-    return queue;
-}
-
-struct Item *maximum(struct PriorityQueue *queue) {
-    // assert(queue->count > 0);
-    return queue->heap[0];
-}
-
-bool empty(struct PriorityQueue *queue) { return queue->count == 0; }
-
-void insert(struct PriorityQueue *queue, struct Item *item) {
-    // assert(queue->count != queue->capacity);
-
-    int i = queue->count;
-    queue->count = i + 1;
-    queue->heap[i] = item;
-
-    for (; i > 0 && queue->heap[(i - 1) / 2]->end > queue->heap[i]->end;
-         i = (i - 1) / 2) {
-        swap(queue->heap[(i - 1) / 2], queue->heap[i]);
-        queue->heap[i]->index = i;
+void siftUp(pqueue_t *q, int i) {
+    while (q->heap[i] < q->heap[(i - 1) / 2]) {
+        swap(&q->heap[i], &q->heap[(i - 1) / 2]);
+        i = (i - 1) / 2;
     }
-
-    queue->heap[i]->index = i;
 }
 
-struct Item *extract_min(struct PriorityQueue *queue) {
-    // assert(!empty(queue));
-
-    struct Item *item = queue->heap[0];
-    --queue->count;
-
-    if (queue->count) {
-        queue->heap[0] = queue->heap[queue->count];
-        queue->heap[0]->index = 0;
-        heapify(queue->heap, queue->count, 0);
-    }
-
-    return item;
+int extract_min(pqueue_t *q) {
+    int min = q->heap[0];
+    q->heap[0] = q->heap[q->count - 1];
+    q->count -= 1;
+    siftDown(q, 0);
+    return min;
 }
 
-void merge(int k, int l[], int *a[], int *result) {
-    int n = 0;
-    for (int i = 0; i < k; ++i)
-        n += l[i];
-
-    struct PriorityQueue *queue = init_priority_queue(n);
-    int *usage = (int *)calloc(k, sizeof(int));
-    for (int i = 0; i < k; ++i)
-        insert(queue, create_item(i, a[i][usage[i]++]));
-
-    for (int i = 0; i < n; ++i) {
-        struct Item *item = extract_min(queue);
-        result[i] = item->end;
-
-        if (l[item->key] > usage[item->key])
-            insert(queue,
-                   create_item(item->key, a[item->key][usage[item->key]++]));
-
-        free(item);
-    }
-
-    free(queue->heap);
-    free(queue);
-    free(usage);
+void insert(pqueue_t *q, int key) {
+    q->count += 1;
+    q->heap[q->count - 1] = key;
+    siftUp(q, q->count - 1);
 }
 
 int main(void) {
     int n, m;
     scanf("%d%d", &n, &m);
-    struct PriorityQueue *queue = init_priority_queue(n);
+    pqueue_t *q = init_pqueue(n);
     int answer = 0;
 
-    for (int i = 0, j = 0; i < m; ++i) {
+    for (int i = 0; i < m; ++i) {
         int t1, t2;
         scanf("%d%d", &t1, &t2);
-
-        if (queue->count < n) {
-            insert(queue, create_item(queue->count, t1 + t2));
-            answer = fmax(answer, t1 + t2);
-        } else {
-            struct Item *item = extract_min(queue);
-            int end = fmax(item->end, t1) + t2;
-            insert(queue, create_item(item->key, end));
-            answer = fmax(answer, end);
-            free(item);
-        }
+        int end = (q->count < n ? t1 : max(extract_min(q), t1)) + t2;
+        insert(q, end);
+        answer = max(answer, end);
     }
 
-    for (int i = 0; i < queue->count; ++i) {
-        answer = fmax(answer, queue->heap[i]->end);
-        free(queue->heap[i]);
-    }
+    for (int i = 0; i < q->count; ++i)
+        answer = max(answer, q->heap[i]);
 
     printf("%d\n", answer);
 
-    free(queue->heap);
-    free(queue);
+    delete_pqueue(q);
 
     return 0;
 }
