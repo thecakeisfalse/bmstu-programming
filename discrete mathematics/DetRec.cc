fbe76765f8d06491e9dfe5f76eb8917b96b5dd70
Semyon Chaykin <52384223+thecakeisfalse@users.noreply.github.com>
2024-06-30 20:36:52 +0300

    [ДМ] Добавлены решения 3-го модуля

---
+++ discrete mathematics/DetRec.cc
@@ -0,0 +1,157 @@
+// ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣄⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+// ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠙⠳⣄⠀⠀⠀⠀⠀⠀⠀⠀
+// ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⠀⠀⠀⠀⠀⠀⠀⠀
+// ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⣠⣤⣤⣤⣤⣄⣀⣀⠀⠀⠀⠀⠀⣠⠎⠀⠀⠀⠀⠀⠀⠀⠀
+// ⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣠⣶⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣶⣖⡉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+// ⠀⠀⢠⣄⣀⣠⣤⣤⣶⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣄⠀⠀⠀⠀⠀⠀⠀⠀
+// ⠀⠀⠀⠉⠻⣿⣿⣿⣿⣿⣿⣯⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣄⠀⠀⠀⠀⠀⠀
+// ⠀⠀⠀⠀⢠⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣆⠀⠀⠀⠀⠀
+// ⠀⠀⠀⢠⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣆⠀⠀⠀⠀
+// ⠀⠀⠀⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡄⠀⠀⠀
+// ⠀⠀⣰⣿⣿⣿⣿⣿⣿⣿⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣄⠀⠀
+// ⠠⣾⣿⢿⣿⣿⣿⣿⡿⠁⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠉⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣯⠉⠀⠀
+// ⠀⠀⠀⢸⣿⣿⣿⡿⠑⠊⣿⣿⡿⠿⠛⠛⠙⠛⣻⣿⣿⣄⡻⠿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡄⠀⠀
+// ⠀⠀⠀⢸⣿⣿⣿⡗⠾⠛⠉⠉⠀⠀⠀⠀⠀⠀⠈⠉⠉⠙⠛⠛⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧⠀⠀
+// ⠀⠀⠀⢸⣿⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⣿⠟⠛⠻⣿⣿⣿⣿⣿⣿⡄⠀
+// ⠀⠀⠀⠀⣿⣿⣿⣇⠀⠀⠀⠀⠀⠀⣀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⣿⢶⡋⠳⢸⣿⣿⣿⣿⣿⣇⠀
+// ⠀⠂⠀⠀⠘⣿⣿⣿⡀⠀⠀⠀⠀⠀⠉⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⣿⡗⠚⢁⣠⣾⣿⣿⣿⣿⣿⣿⠀
+// ⠀⠉⠀⠀⠀⠈⣻⣿⣿⣦⣄⣀⡀⠀⠀⠀⠀⠀⠀⠀⢀⣠⣾⣿⣿⣿⣷⣶⣿⣿⣿⣿⣿⣿⣿⣿⣿⡄
+// ⠀⠀⠀⢺⣿⠤⠿⢿⣿⣿⣿⣿⣿⣿⣷⣶⡄⠀⠀⠀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇
+// ⠀⠀⠀⠀⠀⠀⢀⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⠀⠀⢿⣿⣿⣿⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇
+// ⠀⠀⠀⠀⠀⠀⣸⣿⣿⣿⣿⣿⣿⣿⣿⠟⠁⠀⣀⡠⠜⠋⠁⠀⠀⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠁
+// ⠀⠀⠀⠀⠀⠀⣿⣿⣿⣿⡿⠛⣠⣟⣁⠤⠖⠋⠁⠀⠀⠀⠀⠀⠀⠀⠙⢿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠀
+// ⠀⠀⠀⠀⠀⠀⣿⣿⠟⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⢿⣿⣿⣿⡟⢸⠿⠃⠀
+// ⠀⠀⠀⠀⠀⠀⢸⡏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢦⠀⠀⠀⠀⠀⠀⠀
+// ⠀⠀⠀⠀⠀⠀⡼⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⣆⠀⠀⠀⠀⠀⠀
+//
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using elem = pair<int, string>;
+using Automata = vector<vector<elem>>;
+using Set = set<int>;
+
+void Dfs(const Automata &a, set<int> &c, int q) {
+    if (c.find(q) == c.end()) {
+        c.insert(q);
+        for (auto & [w, label] : a[q]) {
+            if (label == "lambda") {
+                Dfs(a, c, w);
+            }
+        }
+    }
+}
+
+void Det(Automata &a, const vector<string> &x, const vector<int> &final, int q) {
+    vector<Set> hist;
+    vector<vector<Set>> delta(2*a.size());
+
+    auto Closure = [&](const Set &z) {
+        Set c;
+        for (auto & q : z)
+            Dfs(a, c, q);
+        return c;
+    };
+
+    map<pair<int, int>, int> cache;
+    auto Find = [&](int i, int j) {
+        if (cache.find({i, j}) != cache.end()) {
+            return cache[{i, j}];
+        }
+        auto z = delta[i][j];
+        auto it = find(hist.begin(), hist.end(), z);
+        return cache[{i, j}] = distance(hist.begin(), it);
+    };
+
+    Set q0 = Closure({q});
+    vector<Set> s = {q0}, Q = {q0};
+
+    printf("digraph {\n"
+           "\trankdir = LR\n");
+
+    while (!s.empty()) {
+        auto z = s.back();
+        s.pop_back();
+
+        bool isFinal = any_of(z.begin(), z.end(), [&](int v) {
+            return final[v] == 1;
+        });
+
+        hist.push_back(z);
+
+        printf("\t%d [label=\"[", hist.size()-1);
+
+        int i = 1;
+        for (auto v : z) {
+            printf("%d", v);
+            if (i++ != z.size()) putchar(' ');
+        }
+
+        printf("]\", shape=%s]\n", (isFinal ? "doublecircle" : "circle"));
+
+        for (auto &id : x) {
+            Set W;
+            for (auto q : z) {
+                for (auto &[w, label] : a[q]) {
+                    if (label == id) {
+                        W.insert(w);
+                    }
+                }
+            }
+
+            Set z1 = Closure(W);
+            auto it = find(Q.begin(), Q.end(), z1);
+
+            if (it == Q.end()) {
+                Q.push_back(z1);
+                s.push_back(z1);
+            }
+
+            delta[hist.size()-1].push_back(z1);
+        }
+    }
+
+    for (int i = 0; i < hist.size(); ++i) {
+        map<int, int> table;
+        for (int j = 0; j < x.size(); ++j) {
+            if (table.find(Find(i, j)) == table.end()) {
+                table[Find(i, j)] = 1;
+                printf("\t%d -> %d [label=\"%s", i, Find(i, j), x[j].c_str());
+                for (int k = j+1; k < x.size(); ++k) {
+                    if (Find(i, j) == Find(i, k)) {
+                        printf(", %s", x[k].c_str());
+                    }
+                }
+                printf("\"]\n");
+            }
+        }
+    }
+
+    printf("}\n");
+}
+
+int main() {
+    int n, m, u, v, q0;
+    cin >> n >> m;
+
+    Automata a(n);
+    unordered_set<string> used;
+    vector<string> x;
+    for (int i = 0; i < m; ++i) {
+        string s;
+        cin >> u >> v >> s;
+        if (s != "lambda" && used.find(s) == used.end()) {
+            used.insert(s);
+            x.push_back(s);
+        }
+        a[u].emplace_back(v, s);
+    }
+
+    vector<int> final(n);
+    for (auto & f : final) cin >> f;
+    cin >> q0;
+
+    Det(a, x, final, q0);
+}
---
+++ discrete mathematics/MinMealy.cc
@@ -0,0 +1,208 @@
+// ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣄⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+// ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠙⠳⣄⠀⠀⠀⠀⠀⠀⠀⠀
+// ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⠀⠀⠀⠀⠀⠀⠀⠀
+// ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⣠⣤⣤⣤⣤⣄⣀⣀⠀⠀⠀⠀⠀⣠⠎⠀⠀⠀⠀⠀⠀⠀⠀
+// ⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣠⣶⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣶⣖⡉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+// ⠀⠀⢠⣄⣀⣠⣤⣤⣶⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣄⠀⠀⠀⠀⠀⠀⠀⠀
+// ⠀⠀⠀⠉⠻⣿⣿⣿⣿⣿⣿⣯⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣄⠀⠀⠀⠀⠀⠀
+// ⠀⠀⠀⠀⢠⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣆⠀⠀⠀⠀⠀
+// ⠀⠀⠀⢠⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣆⠀⠀⠀⠀
+// ⠀⠀⠀⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡄⠀⠀⠀
+// ⠀⠀⣰⣿⣿⣿⣿⣿⣿⣿⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣄⠀⠀
+// ⠠⣾⣿⢿⣿⣿⣿⣿⡿⠁⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠉⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣯⠉⠀⠀
+// ⠀⠀⠀⢸⣿⣿⣿⡿⠑⠊⣿⣿⡿⠿⠛⠛⠙⠛⣻⣿⣿⣄⡻⠿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡄⠀⠀
+// ⠀⠀⠀⢸⣿⣿⣿⡗⠾⠛⠉⠉⠀⠀⠀⠀⠀⠀⠈⠉⠉⠙⠛⠛⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧⠀⠀
+// ⠀⠀⠀⢸⣿⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⣿⠟⠛⠻⣿⣿⣿⣿⣿⣿⡄⠀
+// ⠀⠀⠀⠀⣿⣿⣿⣇⠀⠀⠀⠀⠀⠀⣀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⣿⢶⡋⠳⢸⣿⣿⣿⣿⣿⣇⠀
+// ⠀⠂⠀⠀⠘⣿⣿⣿⡀⠀⠀⠀⠀⠀⠉⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⣿⡗⠚⢁⣠⣾⣿⣿⣿⣿⣿⣿⠀
+// ⠀⠉⠀⠀⠀⠈⣻⣿⣿⣦⣄⣀⡀⠀⠀⠀⠀⠀⠀⠀⢀⣠⣾⣿⣿⣿⣷⣶⣿⣿⣿⣿⣿⣿⣿⣿⣿⡄
+// ⠀⠀⠀⢺⣿⠤⠿⢿⣿⣿⣿⣿⣿⣿⣷⣶⡄⠀⠀⠀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇
+// ⠀⠀⠀⠀⠀⠀⢀⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⠀⠀⢿⣿⣿⣿⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇
+// ⠀⠀⠀⠀⠀⠀⣸⣿⣿⣿⣿⣿⣿⣿⣿⠟⠁⠀⣀⡠⠜⠋⠁⠀⠀⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠁
+// ⠀⠀⠀⠀⠀⠀⣿⣿⣿⣿⡿⠛⣠⣟⣁⠤⠖⠋⠁⠀⠀⠀⠀⠀⠀⠀⠙⢿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠀
+// ⠀⠀⠀⠀⠀⠀⣿⣿⠟⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⢿⣿⣿⣿⡟⢸⠿⠃⠀
+// ⠀⠀⠀⠀⠀⠀⢸⡏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢦⠀⠀⠀⠀⠀⠀⠀
+// ⠀⠀⠀⠀⠀⠀⡼⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⣆⠀⠀⠀⠀⠀⠀
+//
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using vpair = vector<pair<int, string>>;
+using Automata = vector<vpair>;
+
+const int maxn = 1e5;
+
+int p[maxn], s[maxn];
+
+int find(int v) { return (p[v] == v) ? v : p[v] = find(p[v]); }
+
+void unite(int a, int b) {
+    a = find(a), b = find(b);
+    if (s[a] > s[b])
+        swap(a, b);
+    s[b] += s[a];
+    p[a] = b;
+}
+
+auto split1(const Automata &a, vector<int> &pi) {
+    int m = a.size(), n = m;
+    for (int i = 0; i < m; ++i)
+        p[i] = i, s[i] = 1;
+
+    for (int q1 = 0; q1 < n; ++q1) {
+        for (int q2 = 0; q2 < n; ++q2) {
+            if (find(q1) != find(q2)) {
+                bool eq = true;
+
+                for (int x = 0; x < a[0].size(); ++x) {
+                    if (a[q1][x].second != a[q2][x].second) {
+                        eq = false;
+                        break;
+                    }
+                }
+
+                if (eq) {
+                    unite(q1, q2);
+                    m--;
+                }
+            }
+        }
+    }
+
+    for (int i = 0; i < n; ++i)
+        pi[i] = find(i);
+
+    return m;
+}
+
+auto split(const Automata &a, vector<int> &pi) {
+    int m = a.size(), n = m;
+
+    for (int i = 0; i < m; ++i)
+        p[i] = i, s[i] = 1;
+
+    for (int q1 = 0; q1 < n; ++q1) {
+        for (int q2 = 0; q2 < n; ++q2) {
+            if (pi[q1] == pi[q2] && find(q1) != find(q2)) {
+                bool eq = true;
+
+                for (int x = 0; x < a[0].size(); ++x) {
+                    auto w1 = a[q1][x].first, w2 = a[q2][x].first;
+                    if (pi[w1] != pi[w2]) {
+                        eq = false;
+                        break;
+                    }
+                }
+
+                if (eq) {
+                    unite(q1, q2);
+                    m--;
+                }
+            }
+        }
+    }
+
+    for (int i = 0; i < n; ++i)
+        pi[i] = find(i);
+
+    return m;
+}
+
+auto AufenkampHohn(const Automata &a, int q0) {
+    vector<int> pi(a.size());
+    int m = split1(a, pi);
+
+    while (1) {
+        int m_ = split(a, pi);
+        if (m == m_)
+            break;
+        m = m_;
+    }
+
+    vector<int> delta(a.size()), phi(a.size());
+    int count = 0;
+    for (int i = 0; i < a.size(); ++i) {
+        if (pi[i] == i) {
+            delta[count] = i;
+            phi[i] = count;
+            ++count;
+        }
+    }
+
+    Automata b(m, vpair(a[0].size()));
+    int q0_ = phi[pi[q0]];
+    for (int i = 0; i < m; ++i) {
+        for (int j = 0; j < a[0].size(); ++j) {
+            b[i][j].first = phi[pi[a[delta[i]][j].first]];
+            b[i][j].second = a[delta[i]][j].second;
+        }
+    }
+
+    return make_pair(q0_, b);
+}
+
+void dfs(const Automata &a, int v, vector<int> &used, int &idx) {
+    used[v] = idx++;
+    for (auto &[u, _] : a[v])
+        if (used[u] == -1)
+            dfs(a, u, used, idx);
+}
+
+auto canonize(const Automata &a, int q0) {
+    vector<int> used(a.size(), -1);
+    int idx = 0;
+
+    dfs(a, q0, used, idx);
+
+    Automata b(a), temp(a);
+    for (int i = 0; i < a.size(); ++i) {
+        int q = used[i];
+        for (int j = 0; j < a[0].size(); ++j)
+            temp[q][j].first = used[temp[q][j].first];
+    }
+
+    for (int i = 0; i < a.size(); ++i)
+        b[used[i]] = temp[i];
+
+    return b;
+}
+
+auto readMealy() {
+    int n, m, q0;
+    cin >> n >> m >> q0;
+
+    Automata a(n, vpair(m));
+
+    for (int i = 0; i < n; ++i)
+        for (int j = 0; j < m; ++j)
+            cin >> a[i][j].first;
+
+    for (int i = 0; i < n; ++i)
+        for (int j = 0; j < m; ++j)
+            cin >> a[i][j].second;
+
+    return make_pair(q0, a);
+}
+
+void printMealy(const Automata &a) {
+    printf("digraph {\n");
+    printf("\trankdir = LR\n");
+
+    for (int i = 0; i < a.size(); ++i) {
+        for (int j = 0; j < a[0].size(); ++j) {
+            printf("\t%d -> %d [label=\"%c(%s)\"]\n", i, a[i][j].first, 'a' + j,
+                   a[i][j].second.c_str());
+        }
+    }
+
+    printf("}\n");
+}
+
+int main() {
+    auto [q0, a] = readMealy();
+    auto [q0_, b] = AufenkampHohn(a, q0);
+    auto c = canonize(b, q0_);
+    printMealy(c);
+}
